955bfdc8baf35e63c14d4ebfd89a846b4e013a7c
권대선 <dsun@hanmail.net>
2020-10-19 15:10:34 +0900

    신규

---
+++ dotNet/Controllers/Controller.cs
@@ -0,0 +1,28 @@
+﻿using Atomus.Database;
+using Atomus.Service;
+using System;
+
+namespace Atomus.Message.Controllers
+{
+    internal static class DefaultMessageController
+    {
+        internal static IResponse Search(this ICore core)
+        {
+            IServiceDataSet serviceDataSet;
+
+            serviceDataSet = new ServiceDataSet
+            {
+                ServiceName = core.GetAttribute("ServiceName"),
+                TransactionScope = false
+            };
+            serviceDataSet["Message"].ConnectionName = core.GetAttribute("ConnectionName");
+            serviceDataSet["Message"].CommandText = core.GetAttribute("Procedure");
+            serviceDataSet["Message"].AddParameter("@USER_ID", DbType.Decimal, 18);
+
+            serviceDataSet["Message"].NewRow();
+            serviceDataSet["Message"].SetValue("@USER_ID", Config.Client.GetAttribute("Account.USER_ID") ?? DBNull.Value);
+
+            return core.ServiceRequest(serviceDataSet);
+        }
+    }
+}
---
+++ dotNet/DefaultMessage.cs
@@ -0,0 +1,115 @@
+﻿using Atomus.Diagnostics;
+using Atomus.Message.Controllers;
+using Atomus.Service;
+using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Atomus.Message
+{
+    public class DefaultMessage : IMessageSource
+    {
+        private static DataTable dataTable;
+
+        public DefaultMessage()
+        {
+            IResponse response;
+
+            if (dataTable == null)
+            {
+                try
+                {
+                    response = this.Search();
+
+                    if (response.Status == Status.OK)
+                    {
+                        dataTable = response.DataSet.Tables[0];
+                    }
+                }
+                catch (Exception ex)
+                {
+                    DiagnosticsTool.MyTrace(ex);
+                }
+            }
+        }
+
+        MessageResult IMessageSource.GetMessage(string module, string code, string message)
+        {
+            MessageResult messageResult;
+
+            messageResult = new MessageResult
+            {
+                Message = message
+            };
+
+            if (dataTable == null)
+                return messageResult;
+
+            var a = (from sel in dataTable.AsEnumerable()
+                     where sel.Field<string>("MODULE") == module
+                     && sel.Field<string>("CODE") == code
+                     select new
+                     {
+                         Title = sel.Field<string>("TITLE"),
+                         Text = sel.Field<string>("LANGUAGE_TEXT"),
+                         Icon = sel.Field<string>("ICON"),
+                         Button = sel.Field<string>("BUTTONS")
+                     });
+
+            if (a.Count() >= 1)
+            {
+                messageResult.Title = a.ToList()[0].Title;
+                messageResult.Message = a.ToList()[0].Text;
+                messageResult.MessageBoxIcon = this.GetMessageBoxIcon(a.ToList()[0].Icon);
+                messageResult.MessageBoxButtons = this.GetMessageBoxButtons(a.ToList()[0].Button);
+                messageResult.Result = true;
+            }
+
+            return messageResult;
+        }
+
+        private MessageBoxIcon GetMessageBoxIcon(string messageBoxIcon)
+        {
+            switch (messageBoxIcon)
+            {
+                case "Hand":
+                    return MessageBoxIcon.Hand;
+                case "Stop":
+                    return MessageBoxIcon.Stop;
+                case "Error":
+                    return MessageBoxIcon.Error;
+                case "Question":
+                    return MessageBoxIcon.Question;
+                case "Exclamation":
+                    return MessageBoxIcon.Exclamation;
+                case "Warning":
+                    return MessageBoxIcon.Warning;
+                case "Asterisk":
+                    return MessageBoxIcon.Asterisk;
+                case "Information":
+                    return MessageBoxIcon.Information;
+                default:// "None"
+                    return MessageBoxIcon.None;
+            }
+        }
+        private MessageBoxButtons GetMessageBoxButtons(string messageBoxButtons)
+        {
+            switch (messageBoxButtons)
+            {
+                case "OKCancel":
+                    return MessageBoxButtons.OKCancel;
+                case "AbortRetryIgnore":
+                    return MessageBoxButtons.AbortRetryIgnore;
+                case "YesNoCancel":
+                    return MessageBoxButtons.YesNoCancel;
+                case "YesNo":
+                    return MessageBoxButtons.YesNo;
+                case "RetryCancel":
+                    return MessageBoxButtons.RetryCancel;
+                default:// "OK"
+                    return MessageBoxButtons.OK;
+            }
+        }
+    }
+}
